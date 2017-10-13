8542e68bbf5c7760ce444c3b3607b44910fdc9e1
Kevin Smith <KevinH.Smith@coxinc.com>
2017-10-13 11:32:38 -0400

    Remove SVG tag names from self-closing tags

--- src/self-closing.js
+++ src/self-closing.js
@@ -1,26 +1,12 @@
 'use strict';
 
 const SELF_CLOSING_TAGS = [
-  // HTML
   'area', 'base', 'basefont', 'bgsound', 'br', 'col', 'command', 'embed',
   'frame', 'hr', 'img', 'input', 'isindex', 'keygen', 'link', 'meta', 'param',
-  'source', 'track', 'wbr', '!--',
-  // SVG
-  'animate', 'animateTransform', 'circle', 'cursor', 'desc', 'ellipse',
-  'feBlend', 'feColorMatrix', 'feComposite',
-  'feConvolveMatrix', 'feDiffuseLighting', 'feDisplacementMap',
-  'feDistantLight', 'feFlood', 'feFuncA', 'feFuncB', 'feFuncG', 'feFuncR',
-  'feGaussianBlur', 'feImage', 'feMergeNode', 'feMorphology',
-  'feOffset', 'fePointLight', 'feSpecularLighting', 'feSpotLight', 'feTile',
-  'feTurbulence', 'font-face-format', 'font-face-name', 'font-face-uri',
-  'glyph', 'glyphRef', 'hkern', 'image', 'line', 'missing-glyph', 'mpath',
-  'path', 'polygon', 'polyline', 'rect', 'set', 'stop', 'tref', 'use', 'view',
-  'vkern',
+  'source', 'track', 'wbr',
 ];
 
-const SELF_CLOSING_REGEX = new RegExp(
-  `^(${ SELF_CLOSING_TAGS.join('|') })(?:[\.#][a-zA-Z0-9\u007F-\uFFFF_:-]+)*$`
-);
+const SELF_CLOSING_REGEX = new RegExp(`^(?:${ SELF_CLOSING_TAGS.join('|') })$`, 'i');
 
 function selfClosing(tag) {
   return SELF_CLOSING_REGEX.test(tag);
